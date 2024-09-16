099e550a5619b8decdff1b014fcdcfd516d5a64b
IsithaDev <isitha.dev@gmail.com>
2024-09-16 09:31:31 +0530

    refactor: update AppError class with error code property Added 'code' property to AppError for error categorization.

--- server/src/utils/appError.ts
+++ server/src/utils/appError.ts
@@ -1,12 +1,16 @@
+type Code = "ERROR" | "SENDING_EMAIL_ERROR" | "EMAIL_NOT_VERIFIED";
+
 class AppError extends Error {
   statusCode: number;
   status: string;
+  code?: Code;
   isOperational: boolean;
 
-  constructor(message: string, statusCode: number) {
+  constructor(message: string, statusCode: number, code?: Code) {
     super(message);
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.code = code;
     this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
