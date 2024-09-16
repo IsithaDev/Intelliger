ac7c5b6d457e26fe29da591f4762b61ee56a4cde
IsithaDev <isitha.dev@gmail.com>
2024-09-16 09:40:15 +0530

    feet: integrate Winston logging and enhance global error handling Integrated Winston for logging errors in production. Improved global error handling with database cast error handling. Installed Winston and Nodemailer for loggin errors and handling emails.

--- server/src/middlewares/globalErrorHandler.ts
+++ server/src/middlewares/globalErrorHandler.ts
@@ -1,11 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import winston from "winston";
 
 import AppError from "../utils/appError";
+
+const logger = winston.createLogger({
+  level: "error",
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: "error.log" })],
+});
 
 const sendErrorDev = (err: AppError, res: Response) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    code: err.code,
     stack: err.stack,
     error: err,
   });
@@ -16,14 +24,21 @@ const sendErrorProd = (err: AppError, res: Response) => {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
+      code: err.code,
     });
   } else {
-    console.error("ERROR: ", err);
+    logger.error(err);
     res.status(500).json({
       status: "error",
       message: "Something went very wrong",
     });
   }
+};
+
+const handleCastErrorDB = (err: any) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+
+  return new AppError(message, 400);
 };
 
 function globalErrorHandler(
@@ -34,12 +49,18 @@ function globalErrorHandler(
 ) {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+  err.code = err.code || "ERROR";
 
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   }
   if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    error.message = err.message;
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 }
 
