a2dafac76a743f68db69d77a42490aa9d5b40182
IsithaDev <isitha.dev@gmail.com>
2024-09-17 20:22:59 +0530

    Enhance API Security and Performance Added Helmet for setting various HTTP headers to secure the app Configured Content Security Policy (CSP) with Helmet to prevent XSS attacks Integrated express-mongo-sanitize to prevent NoSQL injection attacks Implemented rate limiting to prevent brute force and DDoS attacks Configured CORS for cross-origin request handling Disabled the 'X-Powered-By' header to obscure app's tech stack Updated middleware order to align with security best practices

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,9 @@
 import express, { NextFunction, Request, Response } from "express";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/user.routes";
@@ -8,10 +12,46 @@ import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ limit: "10kb", extended: true }));
+app.use(helmet());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://trusted-scripts.example.com"],
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: [],
+    },
+  })
+);
+
+app.use(ExpressMongoSanitize());
 
 app.use(cookieParser());
+
+app.disable("x-powered-by");
+
+if (process.env.NODE_ENV === "production") {
+}
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again later.",
+});
+
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ limit: "10kb", extended: true }));
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
